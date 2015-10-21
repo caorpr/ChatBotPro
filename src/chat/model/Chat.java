dae845b580f11c25b7d5d902db4dcf0d9323a01b
caorpr <caorpr@gmail.com>
2015-10-21 13:55:31 -0600

    Done it

--- src/chat/model/Chat.java
+++ src/chat/model/Chat.java
@@ -1,6 +1,50 @@
 package chat.model;
 
+import java.util.ArrayList;
+
 public class Chat 
 {
+	private ArrayList<String> memesList;
+	private ArrayList<String> politicalTopicList;
+	private String userName;
+	private String content;
+	
+	public Chat(String userName)
+	{
+		
+	}
+	
+	private void buildMemesList()
+	{
+		
+	}
+	
+	private void buildPoliticalTopicsList()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	public boolean lenghtChecker(String currentInput)
+	{
+		boolean hasLength = false;
+		if(currentInput != null)
+		{
+			if(currentInput.length() > 0)
+			{
+				hasLength = true;
+			}
+		}
+		
+		return hasLength;
+	}
+	
+	
+	
+	
+	
 	
 }
